3b66dcf790b61885ce2670e4cf8ddc03b98be356
hc24abg <hc24abg@COM-LB154-12.herts.ac.uk>
2026-02-23 15:28:45 +0000

    test import implemented

--- gitTest/src/test/java/Testing.java
+++ gitTest/src/test/java/Testing.java
@@ -2,11 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import org.junit.jupiter.api.Test;
 /**
  *
  * @author harshdeepkumar
  */
 public class Testing {
-    
+    @Test
+    public void complexTest()
+    {
+    Complex num = new Complex(4,5);
+    }
 }
